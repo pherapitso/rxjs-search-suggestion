b3c334c00cc8592f1f2fbf7dcc65a7903a8884ca
Pitso Phera <pitsophera@gmail.com>
2017-05-24 12:43:13 +0200

    Update webpack.config.js
    Add comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
 var path = require('path');
 module.exports = {
+ 
+    //location and name of our entry file
     entry : './app/main.js',
  
+    //where to put our transpiled and bundled code
     output:{
         path: path.resolve(__dirname,'public/'),
         filename:'bundle.js'
     },
  
+     
     module:{
+        /* 
+        find all file with js extension excluding the ones in node module directory 
+        and use babel to transpile javascript code to ES5
+       */
         rules : [
-            { test: /\.(js)$/, use: 'babel-loader'}
+            { 
+         test: /\.(js)$/,
+         exclude:[
+           path.resolve(__dirname, "node_modules")
+         ],
+         loader: 'babel-loader',
+         options: {
+            presets: ["es2015"]
+          }
+       }
         ]
     },
-   
+     
     devServer:{
+    //Server configuration
     publicPath: '/',
     contentBase:'./public'
   
